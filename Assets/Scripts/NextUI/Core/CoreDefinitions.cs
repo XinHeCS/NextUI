95c294181941f63cb430c743c0865f12befd3deb
XinHeCS <xinhe_cs@163.com>
2018-06-22 20:43:27 +0800

    core definitions

--- Assets/Scripts/NextUI/Core/CoreDefinitions.cs
+++ Assets/Scripts/NextUI/Core/CoreDefinitions.cs
@@ -2,41 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoreDefinitions : MonoBehaviour {
-
-    // The possition type of UI element
-    public enum UIFontType
+namespace NextUI
+{
+    public class CoreDefinitions : MonoBehaviour
     {
-        // Normal window: 
-        // Individual window which will not influence other UI elements
-        Normal,
-        // Fixed window:
-        // Container window which will be the parent window of some other UI elements.
-        // Usually as a root window.
-        Fixed,
-        // Pop window:
-        // Used to display information
-        PopUp
-    }
+        // The possition type of UI element
+        public enum UIFormType
+        {
+            // Normal window: 
+            // Individual window which will not influence other UI elements
+            Normal,
+            // Fixed window:
+            // Container window which will be the parent window of some other UI elements.
+            // Usually as a root window.
+            Fixed,
+            // Pop window:
+            // Used to display information
+            PopUp
+        }
 
-    // The display mode od UI element
-    public enum UIShowMode
-    {
-        // Normal display
-        General,
-        // Reserve change
-        ReserveChange,
-        // Hiden all
-        // when in this mode, UI manager will hide all oter UI elements
-        HideOther
-    }
+        // The display mode od UI element
+        public enum UIShowMode
+        {
+            // Normal display
+            General,
+            // Reserve change
+            ReserveChange,
+            // Hiden all
+            // when in this mode, UI manager will hide all oter UI elements
+            HideOther
+        }
 
-    // Transparency of the UI element
-    public enum UILuencyType
-    {        
-        Lucency,
-        Translucence,
-        ImPenetrable,
-        Pentrate
+        // Transparency of the UI element
+        public enum UILuencyType
+        {
+            Lucency,
+            Translucence,
+            ImPenetrable,
+            Pentrate
+        }
     }
 }
