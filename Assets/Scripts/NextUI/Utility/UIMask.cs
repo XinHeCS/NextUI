830dad266ee2a2112ed748f3569b93730018b7c0
XinHeCS <xinhe_cs@163.com>
2018-06-24 23:35:30 +0800

    Add UIMask

--- Assets/Scripts/NextUI/Utility/UIMask.cs
+++ Assets/Scripts/NextUI/Utility/UIMask.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIMask : MonoBehaviour {
+namespace NextUI
+{
+    public class UIMask : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
